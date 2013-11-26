882acc3faf4416a5d02771146b47f3e6fd6b0407
med-help <ulan@medhelp-clinic.ru>
2013-11-26 17:33:46 +0000

    first commit

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :title, :description, :image_url, :price, :product_url, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'd3f6afb1133a0b18996dcad744e7373fc75d866f84149fffd0784ce456186f2418063cd3f91ab037ff4a04edc16b8983e2121f4a3daf20923bd9583707316f29'
